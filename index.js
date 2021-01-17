f545cf9caad91ad1b1ee6b0ce0ba7c17ec9cf074
Teodoro Garcia <tedtke514@gmail.com>
2021-01-16 19:21:24 -0600

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function fetchBooks() {
-
-}
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
+  }
 
 function renderBooks(books) {
+  fetchBooks()
   const main = document.querySelector('main')
   books.forEach(book => {
     const h2 = document.createElement('h2')
@@ -14,3 +17,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+
